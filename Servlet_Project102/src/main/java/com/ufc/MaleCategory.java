f3618cc33cfab24598d6ffff50a950f5b1364869
aasimsyed97 <sayedaasim555@gmail.com>
2023-06-07 13:53:33 +0530

    response sample is done

--- Servlet_Project102/src/main/java/com/ufc/MaleCategory.java
+++ Servlet_Project102/src/main/java/com/ufc/MaleCategory.java
@@ -1,15 +1,22 @@
 package com.ufc;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MaleCategory extends HttpServlet { 
 	 
-	public void service(HttpServletRequest req, HttpServletResponse res) {  
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {  
 		
 		String weight = req.getParameter("men");
-		System.out.println(weight);
+		System.out.println(weight); 
+		
+    PrintWriter out = res.getWriter(); 
+     out.println("<h1>" + weight+ "</h1>");
+     
 	}
 
 }
